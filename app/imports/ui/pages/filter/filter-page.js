51ef1768c5a95b9cceaa8e86240618ddc1b31404
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2017-11-19 12:56:25 -1000

    Little fix to show it in minimongo, subscription

--- app/imports/ui/pages/filter/filter-page.js
+++ app/imports/ui/pages/filter/filter-page.js
@@ -3,14 +3,14 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 import { _ } from 'meteor/underscore';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
-// import { Games } from '/imports/api/interest/GameCollection';
+import { Games } from '/imports/api/interest/GameCollection';
 
 const selectedInterestsKey = 'selectedInterests';
 
 Template.Filter_Page.onCreated(function onCreated() {
   this.subscribe(Interests.getPublicationName());
   this.subscribe(Profiles.getPublicationName());
-  // this.subscribe(Games.getPublicationName());
+  this.subscribe(Games.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(selectedInterestsKey, undefined);
 });
@@ -32,7 +32,7 @@ Template.Filter_Page.helpers({
         function makeInterestObject(interest) {
           return {
             label: interest.name,
-            //selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
+            // selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
           };
         });
   },
