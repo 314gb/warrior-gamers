378078cc084a56c5e3ec5ea15319d92414209e52
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2017-11-20 08:30:29 -1000

    Fixed database issue

--- app/imports/api/base/BaseUtilities.js
+++ app/imports/api/base/BaseUtilities.js
@@ -1,19 +1,11 @@
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
-<<<<<<< HEAD
+import { GamerProfiles } from '/imports/api/profile/GamerProfileCollection';
 import { Games } from '/imports/api/interest/GameCollection';
-=======
-import { GamerProfiles } from '../profile/GamerProfileCollection';
-import { Games } from '../interest/GameCollection';
-
->>>>>>> master
 
 export function removeAllEntities() {
   Profiles.removeAll();
   Interests.removeAll();
-<<<<<<< HEAD
-=======
   GamerProfiles.removeAll();
->>>>>>> master
   Games.removeAll();
 }
