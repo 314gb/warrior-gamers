bc249e7b5f0ecf0ab4c664b7a209bc4eefaab2ae
yizanchen <31357578+yizanchen@users.noreply.github.com>
2017-11-28 08:55:20 -1000

    test

--- app/imports/ui/pages/game/game-page.js
+++ app/imports/ui/pages/game/game-page.js
@@ -1,4 +1,17 @@
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Games } from '/imports/api/interest/GameCollection';
 
+
+Template.Game_Page.onCreated(function onCreated() {
+    this.subscribe(Games.getPublicationName());
+});
+
+Template.Filter_Page.helpers({
+    games() {
+        // Initialize selectedInterests to all of them if messageFlags is undefined.
+        // Find all profiles with the currently selected interests.
+        return GamerProfiles.findAll();
+    },
+
+});
