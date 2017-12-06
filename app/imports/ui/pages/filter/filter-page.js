d6f01cab1262c5c10ea4e29ad10e2ac2397adb07
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2017-12-06 10:27:12 -1000

    Got the games selection to show up events and profiles.

--- app/imports/ui/pages/filter/filter-page.js
+++ app/imports/ui/pages/filter/filter-page.js
@@ -1,10 +1,14 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { _ } from 'meteor/underscore';
+
 import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { GamerProfiles } from '/imports/api/profile/GamerProfileCollection';
+
 import { Interests } from '/imports/api/interest/InterestCollection';
 import { Games } from '/imports/api/interest/GameCollection';
-import { GamerProfiles } from '/imports/api/profile/GamerProfileCollection';
+import { Tags } from '/imports/api/interest/TagsCollection';
+import { Events } from '/imports/api/interest/EventCollection';
 
 const selectedInterestsKey = 'selectedInterests';
 
@@ -12,6 +16,8 @@ Template.Filter_Page.onCreated(function onCreated() {
   this.subscribe(Interests.getPublicationName());
   this.subscribe(Profiles.getPublicationName());
   this.subscribe(Games.getPublicationName());
+  this.subscribe(Tags.getPublicationName());
+  this.subscribe(Events.getPublicationName());
   this.subscribe(GamerProfiles.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(selectedInterestsKey, undefined);
@@ -28,7 +34,16 @@ Template.Filter_Page.helpers({
     const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
     return _.filter(allProfiles, gamerprofile => _.intersection(gamerprofile.games, selectedInterests).length > 0);
   },
-
+  events() {
+    // Initialize selectedInterests to all of them if messageFlags is undefined.
+    if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
+      Template.instance().messageFlags.set(selectedInterestsKey, _.map(Games.findAll(), game => game.name));
+    }
+    // Find all profiles with the currently selected interests.
+    const allEvents = Events.findAll();
+    const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
+    return _.filter(allEvents, event => _.intersection(event.games, selectedInterests).length > 0);
+  },
   games() {
     return _.map(Games.findAll(),
         function makeInterestObject(game) {
